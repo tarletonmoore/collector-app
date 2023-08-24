df1c4b1de3192f45c619d15b4041ca90e1229af2
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-24 16:22:09 -0400

    add game create action if admin, change nav

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,23 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    if current_user.admin
+      @game = Game.new
+      render :new
+    end
+  end
+
+  def create
+    @game = Game.create(
+      title: params[:game][:title],
+      console: params[:game][:console],
+      year: params[:game][:year],
+      price: params[:game][:price],
+      series_id: Series.find_by(title: params[:game][:series_id]).id,
+
+    )
+    redirect_to "/games"
+  end
 end
