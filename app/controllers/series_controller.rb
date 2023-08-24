a3301f79cfb04d4003189b5cf2b116ec17ba90c1
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-24 14:57:02 -0400

    add series show action

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,5 +1,11 @@
 class SeriesController < ApplicationController
   def index
     @series = Series.all
+    render :index
+  end
+
+  def show
+    @series = Series.find_by(id: params[:id])
+    render :show
   end
 end
